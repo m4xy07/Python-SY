e656cf54f82dcc54f2e885570a10819f6c697952
Aman Shaikh <hey@m4xy.org>
2024-08-23 10:37:01 +0530

    Update binarysearch.py

--- Session_3-23-08-2024/binarysearch.py
+++ Session_3-23-08-2024/binarysearch.py
@@ -1,19 +1,21 @@
-l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+l1 = list(map(int, input("Enter a list of numbers: ").split()))
+l1.sort()
 num = int(input("Enter a number: "))
-found = False
+flag = False
 low = 0
 high = len(l1) - 1
 while low <= high:
     mid = (low + high) // 2
     if l1[mid] == num:
-        found = True
+        flag = True
         break
     elif l1[mid] < num:
         low = mid + 1
     else:
         high = mid - 1
-if found:
-    print(f"Number {num} found")
+if flag:
+    print(f"Number {num} found at {mid}")
 else:
     print(f"Number {num} not found")
     
