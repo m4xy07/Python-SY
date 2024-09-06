353adde4257b2de8e9eb3c5cf77628e7eb1b7457
Maximus <hey@m4xy.org>
2024-09-06 08:53:57 +0530

    Create np_linearsearch.py

---
+++ Sem-03/Session_4-06-09-2024/np_linearsearch.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+search_value = 6
+
+found = False
+for element in arr:
+    if element == search_value:
+        found = True
+        break
+
+if found:
+    print("Element found")
+else:
+    print("Element not found")
