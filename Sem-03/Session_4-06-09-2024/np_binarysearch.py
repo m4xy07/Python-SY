e6f453e903dd70c57964cbf6f356ef0a111f22ca
Aman Shaikh <hey@m4xy.org>
2024-09-06 09:12:57 +0530

    Create np_binarysearch.py

---
+++ Sem-03/Session_4-06-09-2024/np_binarysearch.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+arr = np.array([2, 4, 6, 8, 10])
+target = 6
+
+result = binary_search(arr, target)
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found")
